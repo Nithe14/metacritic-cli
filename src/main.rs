90331aff5ec91a74197e741409f4c618d667f8a8
Nithe14 <imnithe@gmail.com>
2022-11-07 18:58:11 +0100

    add score scraping alpha ver

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,22 @@ fn main() {
             .unwrap();
 
     let document = scraper::Html::parse_document(&response);
-    let title_selector = scraper::Selector::parse("h3.product_title>a").unwrap();
+    let items_selector = scraper::Selector::parse("ul.search_results.module>li.result").unwrap();
+    let items = document.select(&items_selector).map(|x| x.inner_html());
     let score_selector = scraper::Selector::parse("div.main_stats>span.metascore_w").unwrap();
+    //let score_selector;
+    //let scores;
+    items.zip(1..11).for_each(|(item, number)| {
+        //let score_selector = scraper:Selector::parse("div.main_stats>span.metascore_w").unwrap();
+        let it = scraper::Html::parse_document(&item);
+        let scores = it.select(&score_selector).map(|x| x.inner_html());
+        scores
+            .zip(1..11)
+            .for_each(|(ite, num)| println!("{}. {}", number, ite.trim()));
+    });
+
+    let title_selector = scraper::Selector::parse("h3.product_title>a").unwrap();
+    //let score_selector = scraper::Selector::parse("div.main_stats>span.metascore_w").unwrap();
     let titles = document.select(&title_selector).map(|x| x.inner_html());
 
     titles.zip(1..11).for_each(|(item, number)| {
