90a174bd1fe3c33c1bac7c0376f34b14bc63b8cf
nithe14 <imnithe@gmail.com>
2022-11-19 23:36:06 +0100

    add first real arguments with clap

--- src/args.rs
+++ src/args.rs
@@ -1,14 +1,12 @@
 use clap::Parser;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
+    /// Item name to search
+    pub name: String,
+
     /// Display only the first item from the result page
     #[arg(short, long)]
-    pub single: String,
-
-    /// Display all items from the first result page
-    #[arg(short, long)]
-    pub all: String,
+    pub single: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 //use clap;
 mod args;
-use std::collections::HashMap;
 
 use args::Args;
 use clap::Parser;
 
 fn main() {
+    let args = Args::parse();
     //let mut hashMap: HashMap<String, String> = HashMap::new();
     let mut scores_vec: Vec<String> = vec![String::new(); 10];
     let mut titles_vec: Vec<String> = vec![String::new(); 10];
     let mut platforms_vec: Vec<String> = vec![String::new(); 10];
 
-    let response =
-        reqwest::blocking::get("https://www.metacritic.com/search/all/witcher%203/results")
-            .unwrap()
-            .text()
-            .unwrap();
+    let response = reqwest::blocking::get(format!(
+        "https://www.metacritic.com/search/all/{}/results",
+        args.name
+    ))
+    .unwrap()
+    .text()
+    .unwrap();
 
     let document = scraper::Html::parse_document(&response);
     let items_selector = scraper::Selector::parse("ul.search_results.module>li.result").unwrap();
@@ -29,13 +31,13 @@ fn main() {
         let scores = it.select(&score_selector).map(|x| x.inner_html());
         scores
             .zip(1..11)
-            .for_each(|(ite, num)| scores_vec[number - 1] = ite.trim().to_owned());
+            .for_each(|(ite, _num)| scores_vec[number - 1] = ite.trim().to_owned());
         //println!("{}. {}", numer, ite.trim()));
 
         let platforms = it.select(&platform_selector).map(|x| x.inner_html());
         platforms
             .zip(0..10)
-            .for_each(|(ite, num)| platforms_vec[number - 1] = ite.trim().to_owned());
+            .for_each(|(ite, _num)| platforms_vec[number - 1] = ite.trim().to_owned());
 
         let title_selector = scraper::Selector::parse("h3.product_title>a").unwrap();
         //let score_selector = scraper::Selector::parse("div.main_stats>span.metascore_w").unwrap();
@@ -49,12 +51,18 @@ fn main() {
         });
     });
 
-    let args = Args::parse();
-    //for i in 0..10 {
-    ////hashMap.insert(titles_vec[i].clone(), scores_vec[i].clone());
-    //println!(
-    //"Title: {}\n Score: {}\n Platform: {}\n\n",
-    //titles_vec[i], scores_vec[i], platforms_vec[i]
-    //)
-    //}
+    if args.single {
+        println!(
+            "Title: {}\nScore: {}\nPlatform: {}\n\n",
+            titles_vec[0], scores_vec[0], platforms_vec[0]
+        )
+    } else {
+        for i in 0..10 {
+            //hashMap.insert(titles_vec[i].clone(), scores_vec[i].clone());
+            println!(
+                "Title: {}\nScore: {}\nPlatform: {}\n\n",
+                titles_vec[i], scores_vec[i], platforms_vec[i]
+            )
+        }
+    }
 }
