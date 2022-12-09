d13570043ea6e467704524fabf51eee40563a4cc
nithe14 <imnithe@gmail.com>
2022-12-09 15:29:55 +0100

    code refactor

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ mod args;
 use args::Args;
 use clap::Parser;
 use colored::Colorize;
+//use std::collections::HashMap;
 use urlencoding::encode;
 
 fn main() {
     let args = Args::parse();
-    //let mut hashMap: HashMap<String, String> = HashMap::new();
+    //let mut hash_map: HashMap<String, Vec<String>> = HashMap::new();          => in the future
     let mut scores_vec: Vec<String> = vec![String::new(); 10];
     let mut titles_vec: Vec<String> = vec![String::new(); 10];
     let mut platforms_vec: Vec<String> = vec![String::new(); 10];
@@ -54,23 +55,23 @@ fn main() {
     let score_selector = scraper::Selector::parse("div.main_stats>span.metascore_w").unwrap();
     let platform_selector = scraper::Selector::parse("div.main_stats>p>span.platform").unwrap();
 
-    items.zip(1..11).for_each(|(item, number)| {
+    items.zip(0..10).for_each(|(item, number)| {
         let it = scraper::Html::parse_document(&item);
         let scores = it.select(&score_selector).map(|x| x.inner_html());
         scores
-            .zip(1..11)
-            .for_each(|(ite, _num)| scores_vec[number - 1] = ite.trim().to_owned());
+            .zip(0..10)
+            .for_each(|(ite, _num)| scores_vec[number] = ite.trim().to_owned());
 
         let platforms = it.select(&platform_selector).map(|x| x.inner_html());
         platforms
             .zip(0..10)
-            .for_each(|(ite, _num)| platforms_vec[number - 1] = ite.trim().to_owned());
+            .for_each(|(ite, _num)| platforms_vec[number] = ite.trim().to_owned());
 
         let title_selector = scraper::Selector::parse("h3.product_title>a").unwrap();
         let titles = document.select(&title_selector).map(|x| x.inner_html());
 
-        titles.zip(1..11).for_each(|(item, number)| {
-            titles_vec[number - 1] = item
+        titles.zip(0..10).for_each(|(item, number)| {
+            titles_vec[number] = item
                 .trim()
                 .replace("<span class=\"title_prefix\">", "")
                 .replace("</span>", "");
@@ -87,6 +88,10 @@ fn main() {
             if titles_vec[i] == "" {
                 break;
             } else {
+                //hash_map.insert(
+                //titles_vec[i].to_owned(),
+                //vec![scores_vec[i].to_owned(), platforms_vec[i].to_owned()],
+                //);
                 if scores_vec[i] == "tbd" || scores_vec[i] == "" {
                     println!(
                         "Title: {}\nScore: {}\nPlatform: {}\n\n",
